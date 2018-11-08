e7f60adfabb596bac2c8ecc61b609540f6310626
Nikolas RAS <nikolas.ras@gmail.com>
2018-11-08 18:01:15 -0300

    Curso em andamento, aula 58

---
+++ workspace/modulo_03/Crypto_Wallet/app/helpers/application_helper.rb
@@ -0,0 +1,19 @@
+module ApplicationHelper
+    def data_br(data_us)
+        data_us.strftime("%d/%m/%Y")
+    end
+    
+    def nome_aplicacao
+        "CRYPTO WALLET APP"
+    end
+    
+    def ambiente_rails
+        if Rails.env.development?
+            "Desenvolvimento"
+        elsif Rails.env.production?
+            "Produção"
+        else
+            "Teste"
+        end
+    end
+end
